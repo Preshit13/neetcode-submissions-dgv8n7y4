1c7581bc64bf83858365b03d57c4c49053832b9f
Preshit Pimple <73753714+Preshit13@users.noreply.github.com>
2026-07-30 13:03:56 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            if( c == '(') stack.push(c);
+            if( c == '{') stack.push(c);
+            if( c == '[') stack.push(c);
+            if( c == ']'){
+            if(stack.isEmpty() || stack.pop() != '[') return false;
+            }
+            if( c == '}'){
+                if(stack.isEmpty() || stack.pop() != '{') return false;
+            }
+            if( c == ')'){
+                if(stack.isEmpty() || stack.pop() != '(') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
